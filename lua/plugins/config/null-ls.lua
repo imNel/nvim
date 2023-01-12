7d286fc2957079866b5461c53fcbfaa43b93924b
Nel Fernandes <46218259+imNel@users.noreply.github.com>
2023-01-12 23:39:13 +0800

    Update null-ls.lua

--- lua/plugins/config/null-ls.lua
+++ lua/plugins/config/null-ls.lua
@@ -1,7 +1,11 @@
 local null_ls = require("null-ls")
 
+local sources = {
+  null_ls.builtins.formatting.prettier.with({
+    extra_filetypes = { "astro" },
+  }),
+}
+
 null_ls.setup({
-  sources = {
-    require("null-ls").builtins.formatting.prettier,
-  },
+  sources = sources
 })
